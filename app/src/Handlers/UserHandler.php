a6e839614e183ea440d3543465e13efaa3e99271
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2023-01-21 08:08:05 +0100

    user can create an account

--- app/src/Handlers/UserHandler.php
+++ app/src/Handlers/UserHandler.php
@@ -2,6 +2,9 @@
 
 namespace Nelwhix\PortfolioApi\Handlers;
 
+use Carbon\CarbonImmutable;
+use Firebase\JWT\JWT;
+use Nelwhix\PortfolioApi\Database;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Respect\Validation\Validator as v;
@@ -59,8 +62,67 @@ class UserHandler
             $this->response->setContent(json_encode([
                 'message' => "Your password and confirmation must match"
             ]));
+
             return;
         }
 
+        $database = new Database();
+
+        $collection = $database->database->users;
+        $result = $collection->insertOne([
+           'name' => $name,
+            'email' => $email,
+            'password' => password_hash($password, PASSWORD_DEFAULT)
+        ]);
+
+        $secret_key = '68V0zWFrS72GbpPreidkQFLfj4v9m3Ti+DXc8OB0gcM=';
+        $date = new CarbonImmutable();
+        $expire_at = $date->addMinutes(6)->getTimestamp();
+        $domainName = "127.0.0.1:8088";
+        $request_data = [
+            'iat' => $date->getTimestamp(),
+            'iss' => $domainName,
+            'nbf' => $date->getTimestamp(),
+            'exp' => $expire_at,
+            'userName' => $name
+        ];
+
+        $token = JWT::encode($request_data, $secret_key, 'HS512');
+
+        $user = $collection->findOne(['_id' => $result->getInsertedId()]);
+
+        $this->response->setStatusCode(Response::HTTP_CREATED);
+
+        $this->response->setContent(json_encode([
+            "message" => "User created successfully",
+            "data" => [
+                "token" => $token,
+                "user" => $user
+            ]
+        ]));
+    }
+
+    public function login() {
+        $email = $this->request->request->get('email');
+
+        if (!$email) {
+            $this->response->setStatusCode(Response::HTTP_UNPROCESSABLE_ENTITY);
+            $this->response->setContent(json_encode([
+                'message' => "Email field is required"
+            ]));
+
+            return;
+        }
+
+        $password = $this->request->request->get('password');
+
+        if (!$password) {
+            $this->response->setStatusCode(Response::HTTP_UNPROCESSABLE_ENTITY);
+            $this->response->setContent(json_encode([
+                'message' => "Password field is required"
+            ]));
+
+            return;
+        }
     }
 }
